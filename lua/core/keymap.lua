60f99b74e2ed9262a6fa1e86619e3654cc6b833c
陈舒豪 <chenshuhao@chenshuhaos-Mac-mini.local>
2024-09-04 16:15:00 +0800

    my nvim config

---
+++ lua/core/keymap.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- ---------- 插入模式 ---------- ---
+keymap.set("i", "jk", "<ESC>")
+
+-- ---------- 视觉模式 ---------- ---
+-- 单行或多行移动
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- ---------- 正常模式 ---------- ---
+-- 窗口
+keymap.set("n", "<leader>sv", "<C-W>v")
+keymap.set("n", "<leader>sh", "<C-W>s")
+
+-- 添加空白行
+keymap.set("n", "<leader>o", "o<ESC>")
+keymap.set("n", "<leader>O", "O<ESC>")
+
+-- 取消高亮
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- ---------- 插件 ---------- ---
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+-- 切换buffer
+keymap.set("n", "<C-L>", ":bnext<CR>")
+keymap.set("n", "<C-H>", ":bprevious<CR>")
