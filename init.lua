96f14e9a95b4ce03ae8cc9077babb3bdce58c2f7
陈舒豪 <chenshuhao@chenshuhaos-Mac-mini.local>
2024-09-30 07:15:20 +0800

    更改为lazy.nvim框架

--- init.lua
+++ init.lua
@@ -1,17 +1,5 @@
 require("core.options")
 require("core.keymap")
 
-require("plugins.plugins-setup")
-
--- 插件
-require("plugins.lualine")
-require("plugins.nvim-tree")
-require("plugins.treesitter")
-require("plugins.lsp")
-require("plugins.cmp")
-require("plugins.comment")
-require("plugins.autopairs")
-require("plugins.bufferline")
-require("plugins.gitsigns")
-require("plugins.telescope")
-require("plugins.markdown-preview")
+-- lazy.nvim
+require("config.lazy")
